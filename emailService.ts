6d91ac8c7018dd115cd5840816ba3f67fe81572f
toandh@vnext.com <toandh@vnext.com>
2022-12-17 17:50:41 +0700

    update

---
+++ emailService.ts
@@ -0,0 +1,9 @@
+import { EmailOptions, EmailProvider, EmailResponse } from "./emailProvider.interface";
+
+class EmailService {
+    constructor(public emailProvider: EmailProvider) {}
+    async sendMail(options: EmailOptions): Promise<EmailResponse> {
+      const result = await this.emailProvider.sendMail(options);
+      return result;
+    }
+  }
